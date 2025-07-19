5d7a8470800c9ce9cafe17cdbeecb785518daaab
Intellifer <intellifercloud@gmail.com>
2025-07-19 22:57:00 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ const express = require("express");
 
 const app = express()
 
-app.use('/home',(req,res) =>{
-    res.send("server is running on 3000 port")
-})
 
-app.use('/user',(req,res) =>{
-    res.send("user data")
-})
+app.use('/user',
+    (res,req,next)=>{
+        console.log("1st user")
+        next()
+},(req,res,next)=>{
+    console.log("2nd user")
+    next()
+},(req,res,next)=>{
+    console.log("3rd user")
+    res.send("3rd user")
+}
+)
+
 
 app.listen(3000 , ()=>{
     console.log("server is running successfully")
