7100425fb3018cd7ed7e1d45ba81c4c8d8dbfc9d
Intellifer <intellifercloud@gmail.com>
2026-01-01 10:25:36 +0530

    completed jwt token setup

--- config/database.js
+++ config/database.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose')
 
-const DBUrl = "mongodb+srv://kamaleshsdev:x2xct2bdiGnoTNrq@dev-cluster.dg5dnud.mongodb.net/"
+const DBUrl = "mongodb+srv://kamaleshsdev:x2xct2bdiGnoTNrq@dev-cluster.dg5dnud.mongodb.net/test"
 
 const connectDB = async()=>{
     await mongoose.connect(DBUrl)
 }
 
 module.exports = {connectDB}
-
-// connectDB().then(()=>{
-//         console.log("db connected")
-//     })
-//     .catch((err)=>{
-//         console.error("db cannot be connected")
-//     }) 
--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const { type } = require('os');
 
-const userSchema = mongoose.SchemaTypes({
+const userSchema = new mongoose.Schema({
     firstName: { type: String},
     lastName: { type: String},
-    emailId: { type: String},
-    password: { type: String},
+    emailId: { type: String , required:true},
+    password: { type: String , required:true},
     age: { type: Number},
     gender: { type: String}
 })
 
+const User = mongoose.model('user',userSchema)
+
+module.exports = User;
